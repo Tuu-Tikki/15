cead58ea26f92e2594c313111d49e1cd882e84c7
Diana Dalakova <diana.dalakova@gmail.com>
2020-12-26 00:31:43 +0100

    Implement the move of stone

--- app/src/main/java/com/example/a15/MainActivity.java
+++ app/src/main/java/com/example/a15/MainActivity.java
@@ -13,10 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final int[] number = {R.string.stone_1,  R.string.stone_2,  R.string.stone_3,  R.string.stone_4,
-                              R.string.stone_5,  R.string.stone_6,  R.string.stone_7,  R.string.stone_8,
-                              R.string.stone_9,  R.string.stone_10, R.string.stone_11, R.string.stone_12,
-                              R.string.stone_13, R.string.stone_14, R.string.stone_15, R.string.empty};
+        final int[] number = {R.integer.stone_1,  R.integer.stone_2,  R.integer.stone_3,  R.integer.stone_4,
+                              R.integer.stone_5,  R.integer.stone_6,  R.integer.stone_7,  R.integer.stone_8,
+                              R.integer.stone_9,  R.integer.stone_10, R.integer.stone_11, R.integer.stone_12,
+                              R.integer.stone_13, R.integer.stone_14, R.integer.stone_15, R.integer.empty};
 
         final TextView[] gameField =
                         {findViewById(R.id.number_1), findViewById(R.id.number_2), findViewById(R.id.number_3), findViewById(R.id.number_4),
@@ -24,10 +24,13 @@ public class MainActivity extends AppCompatActivity {
                         findViewById(R.id.number_9), findViewById(R.id.number_10), findViewById(R.id.number_11), findViewById(R.id.number_12),
                         findViewById(R.id.number_13), findViewById(R.id.number_14), findViewById(R.id.number_15), findViewById(R.id.number_16)};
 
+        //set game field for the first  time
         for (int i=0; i < 16; i++) {
             gameField[i].setText(number[i]);
         };
+         gameField[15].setVisibility(View.INVISIBLE);
 
+        //move the stone in the available free space
         for (int i=0; i < 16; i++) {
             final int counter = i;
             gameField[i].setOnClickListener(new View.OnClickListener() {
@@ -36,9 +39,11 @@ public class MainActivity extends AppCompatActivity {
                     int empty = checkNeighbor(counter, gameField);
                     if (empty > -1) {
                         //int value = Integer.parseInt(gameField[counter].getText().toString());
-                        CharSequence value=gameField[counter].getText();
-                        gameField[counter].setText(R.string.empty);
+                        CharSequence value = gameField[counter].getText();
+                        gameField[counter].setText("0");
+                        gameField[counter].setVisibility(View.INVISIBLE);
                         gameField[empty].setText(value);
+                        gameField[empty].setVisibility(View.VISIBLE);
                     }
                 }
             });
@@ -48,25 +53,25 @@ public class MainActivity extends AppCompatActivity {
     private int checkNeighbor (int i, TextView[] gameField) {
         int empty = -1;
         if ( i - 4 > 0) {
-            if (gameField[i-4].getText().toString().equals(R.string.empty)) {
+            if (Integer.parseInt(gameField[i-4].getText().toString()) == 0) {
                 empty = i - 4;
                 return empty;
             }
         }
         if ( i - 1 > 0) {
-            if (gameField[i-1].getText().toString().equals(R.string.empty)) {
+            if (Integer.parseInt(gameField[i-1].getText().toString()) == 0) {
                 empty = i - 1;
                 return empty;
             }
         }
         if ( i + 1 < 16) {
-            if (gameField[i+1].getText().toString().equals(R.string.empty)) {
+            if (Integer.parseInt(gameField[i+1].getText().toString()) == 0) {
                 empty = i + 1;
                 return empty;
             }
         }
         if ( i + 4 < 16) {
-            if (gameField[i+4].getText().toString().equals(R.string.empty)) {
+            if (Integer.parseInt(gameField[i+4].getText().toString()) == 0) {
                 empty = i + 4;
                 return empty;
             }
--- app/src/main/java/com/example/a15/StonePosition.java
+++ app/src/main/java/com/example/a15/StonePosition.java
@@ -1,8 +1,8 @@
 package com.example.a15;
 
 public class StonePosition {
-    int[] position = {R.string.stone_1,  R.string.stone_2,  R.string.stone_3,  R.string.stone_4,
-                      R.string.stone_5,  R.string.stone_6,  R.string.stone_7,  R.string.stone_8,
-                      R.string.stone_9,  R.string.stone_10, R.string.stone_11, R.string.stone_12,
-                      R.string.stone_13, R.string.stone_14, R.string.stone_15, R.string.empty};
+    int[] position = {R.integer.stone_1,  R.integer.stone_2,  R.integer.stone_3,  R.integer.stone_4,
+                      R.integer.stone_5,  R.integer.stone_6,  R.integer.stone_7,  R.integer.stone_8,
+                      R.integer.stone_9,  R.integer.stone_10, R.integer.stone_11, R.integer.stone_12,
+                      R.integer.stone_13, R.integer.stone_14, R.integer.stone_15, R.integer.empty};
 }
